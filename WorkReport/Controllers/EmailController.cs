6963177ca36b912a85b3112b66bd81b85bc2d508
roy <BetterRoy@163.com>
2022-06-17 16:30:23 +0800

    完善配置文件未写邮件地址，取Redis中

--- WorkReport/Controllers/EmailController.cs
+++ WorkReport/Controllers/EmailController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using WorkReport.Commons.EmailHelper;
+using WorkReport.Commons.RedisHelper.Service;
 using WorkReport.Utility.Filters.Attributes;
 
 namespace WorkReport.Controllers
@@ -8,11 +9,30 @@ namespace WorkReport.Controllers
     public class EmailController : Controller
     {
         private readonly SendServerConfigurationEntity _sendServerConfigurationEntity;
-        public EmailController(IOptionsMonitor<SendServerConfigurationEntity> sendServerConfigurationEntity)
+        private readonly RedisStringService _RedisStringService;
+
+        public EmailController(IOptionsMonitor<SendServerConfigurationEntity> sendServerConfigurationEntity,
+            RedisStringService redisStringService)
         {
             _sendServerConfigurationEntity = sendServerConfigurationEntity.CurrentValue;
+            _RedisStringService = redisStringService;
+
+            InitSendServerConfigurationEntity();
         }
 
+        /// <summary>
+        /// 如果config中为空，则从Redis中取，为防止上传至github私密信息泄露。
+        /// </summary>
+        public void InitSendServerConfigurationEntity()
+        {
+            if (string.IsNullOrEmpty(_sendServerConfigurationEntity.SenderPassword))
+            {
+                string emailKey = "WorkReport.SendServerEmailConfigurationEntity";
+                var redisSemail = _RedisStringService.Get<SendServerConfigurationEntity>(emailKey);
+                _sendServerConfigurationEntity.SenderAccount = redisSemail.SenderAccount;
+                _sendServerConfigurationEntity.SenderPassword = redisSemail.SenderPassword;
+            }
+        }
 
         [CustomAllowAnonymousAttribute]
         public IActionResult Index()
@@ -21,10 +41,10 @@ namespace WorkReport.Controllers
             {
                 Subject = "说点啥当做标题吧！！！",
                 Body = "邮件内容：<BR>博客：<a href='https://blog.csdn.net/hello_mr_anan?type=blog'>这是小王的个人主页</a>",
-                Recipients = new List<string>() { "***@163.com","***@qq.com"},      //收件人
+                Recipients = new List<string>() { "betterroy@163.com", "1121695511@qq.com" },      //收件人
                 SenderAddress = _sendServerConfigurationEntity.SenderAccount,     //发件人
             };
-            
+
             var result = MailHelper.SendMail(mailBodyEntity, _sendServerConfigurationEntity);
 
             return new JsonResult(result);
